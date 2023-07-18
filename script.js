09689e51f27d2b4adb5ed94f3341373ee2c26f9b
UFMAPG <139626553+UFMAPG@users.noreply.github.com>
2023-07-17 21:58:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toESTString();
+	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
 }
 showTime();
 setInterval(function () {
